8404f1fd061c952a2efa157cc54153779c1d817a
mrmoon <abuamaramfreelance@gmail.com>
2021-05-02 02:45:41 +0300

    connected lambda with the gateway

--- Back-End/image_super_resolution.py
+++ Back-End/image_super_resolution.py
@@ -3,18 +3,22 @@ import json
 import os
 
 def lambda_handler(event, context):
+    
     API_URL = 'https://api.deepai.org/api/torch-srgan'
     API_KEY = os.environ['API_KEY']
-    IMAGE_URL = event['image']
+    IMAGE_URL = json.loads(event['body'])['image']
     
     data = urllib.parse.urlencode({'image': IMAGE_URL})
     data = data.encode('ascii')
     res = urllib.request.urlopen(urllib.request.Request(
-            url=API_URL,
-            data=data,
-            headers={'api-key': API_KEY},
-            method='POST'),
-        timeout=5)
-    return json.loads(res.read())
- 
-
+        url=API_URL,
+        data=data,
+        headers={'api-key': API_KEY},
+        method='POST'),
+    timeout=5)
+    
+    return {
+      "isBase64Encoded": False,
+      "statusCode": res.status,
+      "body": json.loads(res.read())['output_url']
+    }
