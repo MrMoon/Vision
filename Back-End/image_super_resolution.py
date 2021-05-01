e0e45ee694890f6fd406b316175f6d1a4d3fc1aa
mrmoon <abuamaramfreelance@gmail.com>
2021-05-01 21:16:55 +0300

    Added Lambda handler

--- Back-End/image_super_resolution.py
+++ Back-End/image_super_resolution.py
@@ -1,21 +1,20 @@
 import urllib.request
 import json
+import os
 
-API_URL = 'https://api.deepai.org/api/torch-srgan'
-# API_KEY 
-IMAGE_URL = 'https://www.drupal.org/files/issues/sample-im-10.png'
-
-data = urllib.parse.urlencode({'image': IMAGE_URL})
-data = data.encode('ascii')
-
-res = urllib.request.urlopen(urllib.request.Request(
-        url=API_URL,
-        data=data,
-        headers={'api-key': API_KEY},
-        method='POST'),
-    timeout=5)
-
-print(res.status)
-print(res.reason)
-print(json.loads(res.read()))
+def lambda_handler(event, context):
+    API_URL = 'https://api.deepai.org/api/torch-srgan'
+    API_KEY = os.environ['API_KEY']
+    IMAGE_URL = event['image']
+    
+    data = urllib.parse.urlencode({'image': IMAGE_URL})
+    data = data.encode('ascii')
+    res = urllib.request.urlopen(urllib.request.Request(
+            url=API_URL,
+            data=data,
+            headers={'api-key': API_KEY},
+            method='POST'),
+        timeout=5)
+    return json.loads(res.read())
  
+
