28232c5205ebbda9e47f8f7fbc266059f8156178
mrmoon <abuamaramfreelance@gmail.com>
2021-05-07 23:03:33 +0300

    Cleaned the image super resolution script

--- AI/image/scripts/image_super_resolution.py
+++ AI/image/scripts/image_super_resolution.py
@@ -2,15 +2,6 @@ import cv2
 from cv2 import dnn_superres
 
 super_resolution = dnn_superres.DnnSuperResImpl_create()
-
-image = cv2.imread("./input.png")
-
 path = "EDSR_x4.pb"
 super_resolution.readModel(path)
-
 super_resolution.setModel("edsr" , 4)
-
-result = super_resolution.upsample(image)
-
-cv2.imwrite("./upscaled.png" , result)
-
